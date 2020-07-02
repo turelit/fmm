daba3482bd8b65d675049b04165496cabf72ffcb
Rafal Turel <rafal.turel@gmail.com>
2020-07-02 07:26:23 +0200

    Sample app

---
+++ sampleApp/sampleApp.Android/obj/Release/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: D8F45CF68E8A9CDA */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	949
+	/* entry-length */
+	.word	232
+	/* value-offset */
+	.word	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 220169
+	.include	"typemap.mj.inc"
---
+++ sampleApp/sampleApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using sampleApp.Models;
+
+namespace sampleApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
